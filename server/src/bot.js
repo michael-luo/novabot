72455d92de066eadcbd75bff9ca6e31ec454da70
Michael Luo <miczluo@gmail.com>
2018-02-04 02:59:53 -0800

    Load bots on startup

--- server/src/bot.js
+++ server/src/bot.js
@@ -1,3 +1,4 @@
+const Setting = require('./models/setting')
 const log = require('./log')
 
 const TwitchBot = require('twitch-bot')
@@ -7,20 +8,42 @@ const bot = new TwitchBot({
 })
 
 bot.on('join', channel => {
-  log.info({ joined: channel })
+  log.info({ botJoined: channel })
 })
 
 bot.on('part', channel => {
-  log.info({ parted: channel })
+  log.info({ botParted: channel })
 })
 
 bot.on('message', chatter => {
-  log.info({ message: chatter })
+  log.info({ receivedMessage: chatter })
   bot.say(chatter.message, chatter.channel)
 })
 
 bot.on('error', err => {
   log.error({ error: err })
 })
+
+bot.irc.on('data', data => {
+  if(data.includes('Welcome')) {
+    // Join channels on instance startup
+    // TODO: Distribute bot instances across nodes, also shouldn't select all from table as it doesn't scale
+    Setting.findAll()
+      .then(settings => {
+        settings.forEach(s => {
+          if(s.botEnabled && s.twitchUsername) {
+            log.info(`Bot loaded settings and joining channel: ${s.twitchUsername}`)
+            bot.join(s.twitchUsername)
+          }
+        })
+      })
+      .catch(err => {
+        log.error({
+          findAllSettingsFailed: err.toString()
+        })
+      })
+  }
+})
+
 
 module.exports = bot
--- server/src/models/setting.js
+++ server/src/models/setting.js
@@ -8,6 +8,8 @@ class Setting extends BaseModel {
     this.twitchID = options.twitchID
     this.userID = options.userID
     this.botEnabled = options.botEnabled
+    this.createdAt = options.createdAt
+    this.twitchUsername = options.twitchUsername
   }
 
   static setBot(twitch_id, enabled) {
@@ -18,8 +20,8 @@ class Setting extends BaseModel {
       })
       .returning('*')
       .then(rows => {
-        log.info({ settingSetBotEnabled: rows })
-        return Setting._fromRows(rows)
+        log.info({ setBotEnabledSettings: rows })
+        return Setting._fromFirstRow(rows)
       })
   }
 
@@ -27,22 +29,43 @@ class Setting extends BaseModel {
     return super.db('settings')
       .where('twitch_id', twitch_id)
       .then(rows => {
-        log.info({ settingRows: rows })
+        log.info({ findByTwitchIDSettings: rows })
+        return Setting._fromFirstRow(rows)
+      })
+  }
+
+  static findAll() {
+    return super.db('settings')
+      .innerJoin('users', 'settings.user_id', 'users.id')
+      .select(['settings.*', 'users.twitch_username'])
+      .then(rows => {
+        log.info({ findAllSettings: rows })
         return Setting._fromRows(rows)
       })
   }
 
-  static _fromRows(rows) {
+  static _fromFirstRow(rows) {
     if(!rows || rows.length == 0) {
       return null
     }
+    return Setting._fromRow(rows[0])
+  }
 
-    const row = rows[0]
+  static _fromRows(rows) {
+    if(!rows || rows.length == 0) {
+      return []
+    }
+    return rows.map(Setting._fromRow)
+  }
+
+  static _fromRow(row) {
     return new Setting({
       id: row.id,
       twitchID: row.twitch_id,
       userID: row.user_id,
-      botEnabled: row.bot_enabled
+      botEnabled: row.bot_enabled,
+      createdAt: row.created_at,
+      twitchUsername: row.twitch_username // Only exists from joins with users
     })
   }
 }
