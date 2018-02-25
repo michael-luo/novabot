eb90d5a0a2771f544ab1a54d37f87c4f263102c1
Michael Luo <miczluo@gmail.com>
2018-02-25 15:34:55 -0800

    build frontend

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="login">
-    <b-button class="btn-twitch" variant="primary" v-on:click="login">
+    <b-button class="btn-twitch" variant="secondary" v-on:click="login">
       <b-img width="25" height="25" left src="static/img/twitch.png" />
       &nbsp; Login with Twitch
     </b-button>
