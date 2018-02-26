fe45e007df3117162b93fbf30c3f54985d5bf01a
Michael Luo <miczluo@gmail.com>
2018-02-25 21:39:42 -0800

    Add stellar command to table

--- client/src/components/Commands.vue
+++ client/src/components/Commands.vue
@@ -8,9 +8,10 @@
 
 <script>
 const commands = [
-  { command: 'See list of commands', description: 'See the list of NovaBot commands.', example: '!commands' },
-  { command: 'Check your balance', description: 'See your current withdrawable balance of cryptocurrencies.', example: '!balance' },
-  { command: 'Donate to streamer', description: 'Donate a specified amount of cryptocurrency to the streamer.', example: '!tip 5' }
+  { command: 'See list of commands.', description: 'See the list of NovaBot commands.', example: '!commands' },
+  { command: 'Check your balance.', description: 'See your current withdrawable balance of cryptocurrencies.', example: '!balance' },
+  { command: 'Donate to streamer.', description: 'Donate a specified amount of cryptocurrency to the streamer.', example: '!tip 5' },
+  { command: 'What is Stellar?', description: 'Provides information about Stellar Lumens cryptocurrency.', example: '!stellar' }
 ]
 
 export default {
