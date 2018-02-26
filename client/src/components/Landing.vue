7d290c2f27025b9efb4fdf2f985889ad9f70ed40
Michael Luo <miczluo@gmail.com>
2018-02-25 22:11:24 -0800

    Add gift to landing page and change navbar url

--- client/src/components/Landing.vue
+++ client/src/components/Landing.vue
@@ -1,6 +1,7 @@
 <template>
 <div class="landing">
   <b-button to="login">Get Started</b-button>
+  <b-img style="padding-top: 22px;" fluid center block src="static/img/donation.gif" />
 </div>
 </template>
 
--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -3,7 +3,7 @@
     <b-navbar toggleable="md" type="dark" variant="dark" fixed="top">
       <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
 
-      <b-navbar-brand to="dashboard">NovaBot</b-navbar-brand>
+      <b-navbar-brand to="/">NovaBot</b-navbar-brand>
 
       <b-collapse is-nav id="nav_collapse">
 
